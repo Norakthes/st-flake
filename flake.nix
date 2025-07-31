33835be3d1bde1b0e23006c568953d71a0604f02
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2025-07-31 16:00:07 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Custom ST build";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = pkgs.st.overrideAttrs (oldAttrs: {
+      patches = [
+        ./patches/st-anysize-0.8.4.diff
+        ./patches/st-boxdraw_c2-0.8.5.diff
+        ./patches/st-scrollback-0.9.2.diff
+        ./patches/st-scrollback-mouse-0.9.2.diff
+      ];
+
+      postPatch = ''
+      cp ${./config.h} config.h
+      '';
+    });
+  };
+}
